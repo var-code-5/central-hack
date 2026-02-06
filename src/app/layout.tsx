ed3f90d3fb78f0ea87084708d7bf2770aa65b9c7
var-code-5 <varshithisgod@gmail.com>
2026-02-06 12:54:24 +0530

    fix: max w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${jetBrainsMono.variable} ${spaceGrotesk.variable} antialiased`}
+        className={`${jetBrainsMono.variable} ${spaceGrotesk.variable} antialiased max-w-screen overflow-x-hidden`}
       >
         <DashboardProvider>
           <ToastProvider>
